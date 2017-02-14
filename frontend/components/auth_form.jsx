8b7e49a47b4bfd5b841f556f73c7f714af655ac9
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-02-14 17:23:26 -0500

    fix miniwarning

--- frontend/components/auth_form.jsx
+++ frontend/components/auth_form.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Link} from 'react-router';
 
 
-class AuthForm {
+export default class AuthForm extends React.Component{
 
   constructor(props){
     super(props);
