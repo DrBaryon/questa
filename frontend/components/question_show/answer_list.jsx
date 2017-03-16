1c7b1040c8ce916a3cf70d557b70a7a3e31862db
Chad Bergman <charles.s.bergman@gmail.com>
2017-03-16 14:19:52 -0400

    readded AnswerList

--- frontend/components/question_show/answer_list.jsx
+++ frontend/components/question_show/answer_list.jsx
@@ -3,31 +3,29 @@ import Answer from './answer';
 
 class AnswerList extends React.Component {
 
-  constructor(props){
-    super(props);
-  }
+ constructor(props){
+   super(props);
+ }
 
-  render(){
-    let answers = Object.keys(this.props.question.answers);
-    debugger
-    return(
-    <div>
-      <div className="answers-list-header">
-          {parseInt(answers.length) + " Answers"}
-      </div>
-      <div className="answers-list">
-        {answers.map(key =>
-          <Answer key={key}
-            question={this.props.question}
-            answer={this.props.question.answers[key]}
-            currentUser={this.props.currentUser}
-            createComment={this.props.createComment}
-            updateQuestion={this.props.updateQuestion}
-            receiveAnswer={this.props.receiveAnswer}/>)}
-      </div>
-    </div>
-    );
-  }
+ render(){
+   let answers = Object.keys(this.props.question.answers);
+   return(
+   <div>
+     <div className="answers-list-header">
+         {parseInt(answers.length) + " Answers"}
+     </div>
+     <div className="answers-list">
+       {answers.map(key =>
+         <Answer key={key}
+           question={this.props.question}
+           answer={this.props.question.answers[key]}
+           currentUser={this.props.currentUser}
+           createComment={this.props.createComment}
+           receiveAnswer={this.props.receiveAnswer}/>)}
+     </div>
+   </div>
+   );
+ }
 }
 
 export default AnswerList;
