e8db28eedd71585f9f4c498a4b08e6f30436268a
Charles Bergman <Charles Bergman>
2017-03-16 18:00:20 -0400

    fixed querybar

--- frontend/components/question_index/query_bar.jsx
+++ frontend/components/question_index/query_bar.jsx
@@ -2,21 +2,23 @@ import React from 'react';
 import {Link, withRouter} from 'react-router';
 
 class QueryBar extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {title: "", description: "", showDescription: false,
+   constructor(props){
+     super(props);
+     this.state = {title: "", description: "", showDescription: false,
       expanded: false};
-    this.askQuestion = this.askQuestion.bind(this);
-    this.toggleExpand = this.toggleExpand.bind(this);
-    this.toggleDescriptionField = this.toggleDescriptionField.bind(this);
-    this.goHome = this.goHome.bind(this);
-    this.handleLogout = this.handleLogout.bind(this);
-    this.redirectIfLoggedOut = this.redirectIfLoggedOut.bind(this);
-  }
+      expanded: false, displayedQuestions: []};
+     this.askQuestion = this.askQuestion.bind(this);
+    this.filterQuestions = this.filterQuestions.bind(this);
+     this.toggleExpand = this.toggleExpand.bind(this);
+     this.toggleDescriptionField = this.toggleDescriptionField.bind(this);
+     this.goHome = this.goHome.bind(this);
+     this.handleLogout = this.handleLogout.bind(this);
+     this.redirectIfLoggedOut = this.redirectIfLoggedOut.bind(this);
+   }
 
-  componentDidMount(){
-    this.setState({
-      expanded: false
+   componentDidMount(){
+     this.setState({
+       expanded: false
     });
   }
 
@@ -25,22 +27,35 @@ class QueryBar extends React.Component {
     const question = this.state;
     this.props.createQuestion(question).then(action => {
       this.props.router.push(`/${action.question.id}`);
+     });
+   }
+
+  filterQuestions(){
+    let searchTerm = this.state.title;
+    let displayedQuestions = this.props.questions.filter((question) => {
+      return question.title.includes(searchTerm);
+    });
+    this.setState({
+      displayedQuestions: displayedQuestions
     });
   }
 
-  clear(field) {
-		return e => this.setState({
-      [field]: ""
-    });
-	}
+   clear(field) {
+     return e => this.setState({
+       [field]: ""
+     });
+   }
 
-  update(field) {
+   update(field) {
 		return e => this.setState({
       [field]: e.currentTarget.value
     });
-	}
+		return e => {
+      this.setState({[field]: e.currentTarget.value}, this.filterQuestions);
+    };
+   }
 
-  toggleExpand(){
+   toggleExpand(){
     this.setState({
       expanded: !this.state.expanded
     });
@@ -63,8 +78,8 @@ class QueryBar extends React.Component {
   }
 
   redirectIfLoggedOut() {
-		this.props.router.push("/login");
-	}
+   this.props.router.push("/login");
+ }
 
   render(){
     let descriptionField = "";
@@ -78,28 +93,38 @@ class QueryBar extends React.Component {
         <div className="expanded-query-bar-container">
           <form className = "expanded-query-bar" onSubmit={this.askQuestion} onClick={null}>
               <div className="query-bar-logo" onClick={this.goHome}>Questa</div>
-              <div className="expanded-ask-bar">
-                <textarea rows="1" onChange={this.update("title")} />
-                {descriptionField}
-              </div>
-              <div className="expanded-tool-bar">
-                <input type="submit" value="Just Do It!"/>
-                <button type="button" onClick={this.toggleDescriptionField}>Add Description</button>
-              </div>
-          </form>
-          <div className="grey-modal" onClick={this.toggleExpand} />
-        </div>
+               <div className="expanded-ask-bar">
+                 <textarea rows="1" onChange={this.update("title")} />
+                 {descriptionField}
+                <ul className="search-results">
+                  {this.state.displayedQuestions.map((question) =>
+                    <li key={question.id}>
+                      <a href={"/#/" + question.id}>{question.title}</a>
+                    </li>
+                  )}
+                </ul>
+               </div>
+               <div className="expanded-tool-bar">
+                 <input type="submit" value="Just Do It!"/>
+                 <button type="button" onClick={this.toggleDescriptionField}>Add Description</button>
+               </div>
+           </form>
+
+
+           <div className="grey-modal" onClick={this.toggleExpand} />
+         </div>
 
       );
     }
     return (
-      <form className = "query-bar" onSubmit={this.askQuestion}>
-        <div className="query-bar-logo" onClick={this.goHome}>Questa</div>
-        <div className="ask-bar" onClick={this.toggleExpand}>
+       <form className = "query-bar" onSubmit={this.askQuestion}>
+         <div className="query-bar-logo" onClick={this.goHome}>Questa</div>
+         <div className="ask-bar" onClick={this.toggleExpand}>
           <textarea rows="1" onChange={this.update("title")} />
-          <input type="submit" value = "Ask Question"/>
-        </div>
-        <div className="nav-bar">
+          <textarea rows="1" />
+           <input type="submit" value = "Ask Question"/>
+         </div>
+         <div className="nav-bar">
           <button>Read</button>
           <button>Answer</button>
           <button>Notifications</button>
@@ -114,6 +139,4 @@ class QueryBar extends React.Component {
   }
 }
 
-export default withRouter(QueryBar);
-
-//<img width="20" height="20" src="https://qsf.ec.quoracdn.net/-3-images.feed2.svg-26-27d3066097b99f1c.svg"/>
+export default withRouter(QueryBar)
