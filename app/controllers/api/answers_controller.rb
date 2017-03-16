0f31aa64be31cb88bee7cc378df984dcde015fb6
Chad Bergman <charles.s.bergman@gmail.com>
2017-03-16 16:23:03 -0400

    fixed answer destroy route

--- app/controllers/api/answers_controller.rb
+++ app/controllers/api/answers_controller.rb
@@ -5,14 +5,12 @@ class Api::AnswersController < ApplicationController
 
   def create
     @answer = current_user.answers.create!(answer_params)
-    @answer.upvotes = 0;
     render "api/answers/show"
   end
 
   def destroy
     @answer = current_user.answers.find(params[:id])
     @answer.delete
-    render "api/answers/show"
   end
 
   private
