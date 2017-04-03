1b163113cf940bb595f81b106b990a8568e61bb7
App Academy Student <wancongzhang@gmail.com>
2017-04-03 12:33:42 -0400

    added toggle link and actions for topic feed

--- frontend/util/session_api_util.js
+++ frontend/util/session_api_util.js
@@ -22,3 +22,11 @@ export const logout = () => {
     url: '/api/session'
   });
 };
+
+export const updateUser = (user) => {
+  return $.ajax({
+    method: 'PATCH',
+    url: `/api/user/${user.id}`,
+    data: user
+  });
+}
